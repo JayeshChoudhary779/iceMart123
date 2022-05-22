96a6636c6d69e00deb3f0ca3a765b54f854d1ff3
jayesh2906 <jayesh.choudhary2906@gmail.com>
2022-05-22 16:29:17 +0530

    make it better

--- config/keys.js
+++ config/keys.js
@@ -2,7 +2,7 @@ import dev from "./dev.js";
 import prod from "./prod.js";
 
 let key = {};
-if (process.env.NODE_ENV == "production") {
+if (process.env.MONGOURI) {
   key = prod;
 } else {
   key = dev;
